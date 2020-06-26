cec239c52eb4b7032129723b55f6ef0be8862a0c
jeremybatesdc <jeremy.bates@gmail.com>
2020-06-26 06:27:56 -0400

    PWA

--- src/main.js
+++ src/main.js
@@ -4,6 +4,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./ts_test";
 
-// navigator.serviceWorker.register("/sw.js");
+navigator.serviceWorker.register("/sw.js");
 
 createApp(App).mount("#app");
